a834006d0041981c2db3a92bc50bc081b9020db6
joy <mahadi.hasan@mobioapp.com>
2022-06-19 12:44:40 +0600

    BACKEND API ADDED

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -3,29 +3,31 @@ import React, {useState} from "react";
 
 export default function SignInForm() {
 
-    const [username,setUsername] = useState("")
+    const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
      async function login(){
-        console.log(username,password)
-         let item = {username,password}
-        let result = await fetch("http://localhost:8080/api/auth/signin",{
+        console.log(email,password)
+         let item = {email,password}
+        let result = await fetch("https://spring-boot--signin-jwt.herokuapp.com/user/signIn",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json",
-                "Accept":"application/json"
+                "Accept":"application/json",
+                "Content-Length":"<calculated when request is sent>",
+                "Host":"<calculated when request is sent>"
             },
             body:JSON.stringify(item)
         })
          result = await result.json()
-         console.log(result)
-         localStorage.setItem("userInfo",JSON.stringify(result))
+         console.log("Result######"+result)
+         localStorage.setItem("token",JSON.stringify(result))
          alert(JSON.stringify(result))
     }
     return (
         <div className="col-sm-6 offset-sm-3">
             <form className="form block">
                <div className="">
-                       <input onChange={(e)=>(setUsername(e.target.value))} className="form-control" type="text" placeholder="Email"/>
+                       <input onChange={(e)=>(setEmail(e.target.value))} className="form-control" type="text" placeholder="Email"/>
                    <br/>
                        <input onChange={(e)=>(setPassword(e.target.value))} className="form-control" placeholder="Password" type="password"/>
                </div>
