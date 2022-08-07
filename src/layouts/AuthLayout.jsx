8458db0043ddfe09254f6b8f66fb27309aea8136
mahadi.joy <mahadihasanjoy95@gmail.com>
2022-08-07 08:31:24 +0600

    AUTH LAYOUT NAVIGATION ADDED

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import AuthNavBar from "../components/navbar/AuthNavBar";
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {LOGIN, LOGOUT} from "../redux/actions/Actions";
 
 function AuthLayout(props) {
-    return (
+    const navigate = useNavigate()
+    const dispatch = useDispatch();
+    let logout = useSelector((state) => state.authReducer.logout);
+    console.log(logout)
+    useEffect(()=>{
+        if (localStorage.getItem("token") === null || localStorage.getItem("token").length===0) {
+            dispatch(LOGOUT())
+        }
+        else{
+            dispatch(LOGIN())
+        }
+        if (!logout)
+            navigate("/dashboard")
+    })
+    return(
         <div>
             <AuthNavBar/>
             <h1 className={"text-center"}>Auth Layout</h1>
